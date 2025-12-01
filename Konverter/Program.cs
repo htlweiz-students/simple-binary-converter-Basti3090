375d98f0f2464aebeac89e699f37f5450c0382f7
Student <foobar@sr.htlweiz.at>
2025-12-01 11:24:34 +0100

    DONE

--- Konverter/Program.cs
+++ Konverter/Program.cs
@@ -1,6 +1,11 @@
-﻿namespace Convert;
+﻿namespace Konverter;
 
-public class Convert{
-    
+public class BinaryConverter
+{
+    public static uint ToDecimal(string b)
+    {
+        return Convert.ToUInt32(b, 2);
+    }
 }
+
 
---
+++ Program.cs
@@ -0,0 +1,8 @@
+using Konverter;
+
+class Program {
+    
+    static void Main(string[] argv) {
+        Console.Write("Enter random or specific binary numbers that will be converted to decimal: ")
+    }
+}
